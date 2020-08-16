e50c39f18773eb95fd8076a215f18f0cc55360e6
Ryan Doughty <rdought1@nd.edu>
2020-08-16 13:27:30 -0400

    google utility updates

--- image/shared/google_utility.py
+++ image/shared/google_utility.py
@@ -6,26 +6,17 @@ import io
 import socket
 
 
-def _get_credentials_from_service_account_info(credentials):
-    """ Return credentials given service account file and assumptions of scopes needed """
-    service_account_info = credentials
-    # Scopes: https://developers.google.com/identity/protocols/googlescopes
-    SCOPES = ['https://www.googleapis.com/auth/drive']
-    credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
-    return (credentials)
-
-
 def establish_connection(credentials):
     """ Create a connection to the Google API """
-    timeout_in_sec = 5  # timeout for build process
-    socket.setdefaulttimeout(timeout_in_sec)
-    credentials = _get_credentials_from_service_account_info(credentials)
-    return build('drive', 'v3', credentials=credentials)
+    socket.setdefaulttimeout(5)  # timeout in seconds for build process
+    scopes = ['https://www.googleapis.com/auth/drive']
+    google_creds = service_account.Credentials.from_service_account_info(credentials, scopes=scopes)
+    return build('drive', 'v3', credentials=google_creds)
 
 
 def download_file(connection, file_id, local_file_name) -> bool:
-    downloaded = True
     try:
+        downloaded = True
         request = connection.files().get_media(fileId=file_id)
         fh = io.FileIO(local_file_name, 'wb')
         downloader = MediaIoBaseDownload(fh, request)
