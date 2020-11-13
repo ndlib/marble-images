17c8c4546b8d09d878807ba75529ad5aad870998
Ryan Doughty <rdought1@nd.edu>
2020-11-12 22:11:34 -0500

    lock step work and delete

--- image/systems/rbsc.py
+++ image/systems/rbsc.py
@@ -12,7 +12,7 @@ counter = 0
 
 
 def _list_changes() -> list:
-    kwargs = {'Bucket': config.PROCESS_BUCKET, 'Prefix': 'rbsc/'}
+    kwargs = {'Bucket': config.PROCESS_BUCKET, 'Prefix': config.JSON_PROCESS_DIR}
     data = []
     while True:
         resp = aws_utility.list_files(**kwargs)
@@ -25,7 +25,6 @@ def _list_changes() -> list:
                 data.append(json.load(json_file))
             json_file.close()  # force close before deletion
             os.remove(local_file)
-            aws_utility.delete_file(config.PROCESS_BUCKET, obj["Key"])
         try:
             kwargs['ContinuationToken'] = resp['NextContinuationToken']
         except KeyError:
@@ -40,6 +39,7 @@ def _reprocess_image(queue: Queue) -> None:
         tif_filename = f"{os.path.splitext(tif_filename)[0]}.tif"
         local_file = f"TEMP_{os.path.basename(img_data['key'])}"
         aws_utility.download_file(config.RBSC_BUCKET, img_data["key"], local_file)
+        print(f'Processing {local_file}')
         image = _preprocess_image(local_file)
         if image:
             image.tiffsave(tif_filename, tile=True, pyramid=True, compression=config.COMPRESSION_TYPE,
@@ -49,6 +49,8 @@ def _reprocess_image(queue: Queue) -> None:
             aws_utility.upload_file(config.IMAGE_BUCKET, key, tif_filename)
             os.remove(tif_filename)
             os.remove(local_file)
+            aws_utility.delete_file(config.PROCESS_BUCKET, f"{config.JSON_PROCESS_DIR}{os.path.splitext(tif_filename)[0]}.json")
+            print(f'Completed {local_file}')
         global counter
         counter += 1
         queue.task_done()
@@ -63,16 +65,13 @@ def _preprocess_image(local_file: str) -> Image:
                 shrink_by = image.height / config.DEFAULT_MAX_HEIGHT
             else:
                 shrink_by = image.width / config.DEFAULT_MAX_WIDTH
-            print(f'Resizing original image by: {shrink_by}')
-            print(f'Original image height: {image.height}')
-            print(f'Original image width: {image.width}')
+            print(f'Resizing {local_file} by: {shrink_by}')
+            print(f'Original {local_file} image height: {image.height}')
+            print(f'Original {local_file} image width: {image.width}')
             image = image.shrink(shrink_by, shrink_by)
     except Error as pye:
         image = None
         print(f"VIPs error - {pye.message}")
-    except Exception as e:
-        image = None
-        print(f"General exception - {e}")
     return image
 
 
