3a075efd59462cf4f72e2382e8113764ac09af61
Ryan Doughty <rdought1@nd.edu>
2021-03-25 09:44:40 -0400

    rbsc appsync

--- image/systems/rbsc.py
+++ image/systems/rbsc.py
@@ -1,5 +1,4 @@
 import os
-import json
 from queue import Queue
 import threading
 from pyvips import Image, Error
@@ -12,41 +11,23 @@ import time
 counter = 0
 
 
-def _list_changes() -> list:
-    kwargs = {'Bucket': config.PROCESS_BUCKET, 'Prefix': config.JSON_PROCESS_DIR}
-    data = []
-    while True:
-        resp = aws_utility.list_files(**kwargs)
-        if resp['KeyCount'] == 0:
-            return {}
-        for obj in resp['Contents']:
-            data.append(obj['Key'])
-        try:
-            kwargs['ContinuationToken'] = resp['NextContinuationToken']
-        except KeyError:
-            break
-    return data
-
-
 def _reprocess_image(queue: Queue) -> None:
     while not queue.empty():
         img_data = queue.get()
-        img_data = _retrieve_file_data(img_data)
-        tif_filename = os.path.basename(img_data["key"])
-        tif_filename = f"{os.path.splitext(tif_filename)[0]}.tif"
-        local_file = f"TEMP_{os.path.basename(img_data['key'])}"
-        aws_utility.download_file(config.RBSC_BUCKET, img_data["key"], local_file)
+        tif_filename = os.path.basename(img_data["filePath"])
+        local_file = f"TEMP_{os.path.basename(img_data['filePath'])}"
+        s3_info = f"s3://{img_data['sourceBucketName']}/"
+        src_img = img_data["sourceFilePath"].replace(s3_info, '')
+        aws_utility.download_file(img_data["sourceBucketName"], src_img, local_file)
         logger.debug(f'Processing {local_file}')
         image = _preprocess_image(img_data, local_file)
         if image:
             image.tiffsave(tif_filename, tile=True, pyramid=True, compression=config.COMPRESSION_TYPE,
                 tile_width=config.PYTIF_TILE_WIDTH, tile_height=config.PYTIF_TILE_HEIGHT, \
                 xres=config.DPI_VALUE, yres=config.DPI_VALUE) # noqa
-            key = f"{img_data['path']}{tif_filename}"
-            aws_utility.upload_file(config.IMAGE_BUCKET, key, tif_filename)
+            aws_utility.upload_file(config.IMAGE_BUCKET, img_data["filePath"], tif_filename)
             os.remove(tif_filename)
         os.remove(local_file)
-        aws_utility.delete_file(config.PROCESS_BUCKET, f"{config.JSON_PROCESS_DIR}{os.path.splitext(tif_filename)[0]}.json")
         logger.debug(f'Completed {local_file}')
         global counter
         counter += 1
@@ -54,21 +35,13 @@ def _reprocess_image(queue: Queue) -> None:
     # print(f"{image.get_fields()}")  # image fields, including exif
 
 
-def _retrieve_file_data(remote_file: str) -> dict:
-    local_file = os.path.basename(remote_file)
-    aws_utility.download_file(config.PROCESS_BUCKET, remote_file, local_file)
-    with open(local_file) as json_file:
-        data = json.load(json_file)
-    os.remove(local_file)
-    return data
-
-
 def _preprocess_image(img_data: dict, local_file: str) -> Image:
     try:
         image = Image.new_from_file(local_file, access='sequential')
         max_height = config.DEFAULT_MAX_HEIGHT
         max_width = config.DEFAULT_MAX_WIDTH
-        if img_data.get("copyrightStatus", "").lower() == "copyright":
+        if img_data["copyrightStatus"] and \
+                img_data["copyrightStatus"] == "copyright":
             max_height = config.COPYRIGHT_MAX_HEIGHT
             max_width = config.COPYRIGHT_MAX_WIDTH
         if image.height > max_height or image.width > max_width:
@@ -89,12 +62,11 @@ def _preprocess_image(img_data: dict, local_file: str) -> Image:
     return image
 
 
-def process_rbsc_changes():
+def process_rbsc_changes(data: list):
     jobs = Queue()
-    logger.info("GATHERING RBSC IMAGES TO PROCESS")
-    for img_data in _list_changes():
+    for img_data in data:
         jobs.put(img_data)
-    logger.info(f"{jobs.qsize()} IMAGES TO PROCESS")
+    logger.info(f"{jobs.qsize()} {config.S3.upper()} IMAGES TO PROCESS")
 
     start_time = time.time()
     for i in range(config.MAX_THREADS):
@@ -105,7 +77,3 @@ def process_rbsc_changes():
     elapsed_time = end_time - start_time
     logger.info(f"RBSC PROCESSED {counter} IMAGES")
     logger.info(f"ELAPSED TIME = {elapsed_time} seconds")
-
-
-if __name__ == "__main__":
-    process_rbsc_changes()
